eb3e7fe816cd6926960c47daf5531e4dc82c330a
bantee-sharma <banteesharma9@gmail.com>
2025-03-22 22:21:50 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -5,10 +5,11 @@ from langchain_community.document_loaders import PyPDFLoader, PyMuPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import RetrievalQA
 load_dotenv()
 llm = GoogleGenerativeAI(model = "gemini-2.0-flash")
 
-loader = PyMuPDFLoader("SQL Revision Notes.pdf")
+loader = PyPDFLoader("SQL Revision Notes.pdf")
 document = loader.load()
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=150)
@@ -18,10 +19,21 @@ embeddings = HuggingFaceEmbeddings()
 
 db = FAISS.from_documents(docs,embeddings)
 
-query = "What is primary key?"
-docs = db.similarity_search(query,k=1)
+retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": 1})
 
-for i in docs:
-    print(i.page_content.replace("\n"," "))
-    print("-----------------")
+prompt = PromptTemplate(
+    template= "You are an expert AI assitant. Answer the based on provide document:\n {context} question:{query} \n Answer: ",
+    input_variables=["context","query"]
+)
+
+qa_chain = RetrievalQA.from_chain_type(llm,retriever = retriever)
+
+def ask_question(query):
+    response = qa_chain.run(query)
+    return response
+
+query = "What is the primary key?"
+ans = ask_question(query)
+
+print(ans)
 
