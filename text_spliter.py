b2392388d9d144c095a3e000369ec06039285caa
bantee-sharma <banteesharma9@gmail.com>
2025-04-26 21:42:08 +0530

    Delete text_spliter.py

--- text_spliter.py
+++
@@ -1,9 +0,0 @@
-from langchain_text_splitters import CharacterTextSplitter,RecursiveCharacterTextSplitter
-
-rec_split = RecursiveCharacterTextSplitter(chunk_overlap=2,chunk_size=10)
-
-char_slpit = CharacterTextSplitter(separator=" ")
-
-print(char_slpit.split_text('abcdefghijklmnopqrstuvwxyz'))
-print(char_slpit.split_text('abcdef ghijklmnop qrstuvwxyz'))
-print(rec_split.split_text('abcdef ghijklmnop qrstuvwxyz'))
